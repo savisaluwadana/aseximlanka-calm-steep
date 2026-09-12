b487316ec6da22d4922a857edfa62f78f85c55ba
savi saluwadana <82082138+savisaluwadana@users.noreply.github.com>
2026-09-12 09:05:02 +0530

    style: load image-free homepage motion layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./premium-motion.css";
+import "./no-image-home.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
